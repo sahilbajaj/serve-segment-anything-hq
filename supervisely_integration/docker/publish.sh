ce6354db06eb8b28ba9e500ac60b5f753cd31a28
Sahil Bajaj <bajaj@Sahils-MacBook-Pro-2.local>
2025-04-03 15:32:28 +0530

    Change docker repo

--- supervisely_integration/docker/publish.sh
+++ supervisely_integration/docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/segment-anything-hq:1.0.1 . && \
-docker push supervisely/segment-anything-hq:1.0.1
+docker build -t faaya/segment-anything-hq:1.0.1 . && \
+docker push faaya/segment-anything-hq:1.0.1
